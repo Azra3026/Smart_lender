8b17db23f2804115ecd6472d00e2782efd71940e
Azra <syedazraaquib@google.com>
2026-07-06 22:52:47 +0530

    uni

--- Datasets/univariate.py
+++ Datasets/univariate.py
@@ -1,7 +1,7 @@
+import pandas as pd
 import matplotlib.pyplot as plt
- 
 import seaborn as sns
-
+data = pd.read_csv('your_dataset.csv') 
 # plotting using histplot instead of distplot
 plt.figure(figsize=(12, 5))
 
@@ -10,7 +10,5 @@ sns.histplot(data['ApplicantIncome'], color='r', kde=True)
 
 plt.subplot(122)
 sns.histplot(data['Credit_History'], kde=True)
-
-df = pd.read_csv('your_dataset.csv') 
 
 plt.show()
